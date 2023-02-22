ec168c10492191a295277a5437f2498bab43be5c
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-02-22 15:15:37 -0800

    added funcitonality

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,223 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var saveButton = $(".saveBtn");
+  var description = $(".description");
+
+  function setBtn(num, obj, timeOfDay, string) {
+    saveButton[num].addEventListener('click', function() {
+      obj = {
+        timeOfDay: timeOfDay,
+        TODO: description[num].value.trim()
+      }
+      localStorage.setItem(string, JSON.stringify(obj))
+    })
+  }
+
+  setBtn(0, eight, 8, 'eight');
+  setBtn(1, nine, 9, 'nine');
+  setBtn(2, ten, 10, 'ten');
+  setBtn(3, eleven, 11, 'eleven');
+  setBtn(4, twelve, 12, 'twelve');
+  setBtn(5, one, 1, 'one');
+  setBtn(6, two, 2, 'two');
+  setBtn(7, three, 3, 'three');
+  setBtn(8, four, 4, 'four');
+  setBtn(9, five, 5, 'five');
+
+  function getLastChange(string, obj, num) {
+    var lastEdit = JSON.parse(localStorage.getItem(string));
+    console.log(lastEdit)
+    if(lastEdit !== null) {
+      description[num].textContent = lastEdit.TODO;
+    }
+  }
+
+  function init() {
+    getLastChange('eight', eight, 0);
+    getLastChange('nine', nine, 1);
+    getLastChange('ten', ten, 2);
+    getLastChange('eleven', eleven, 3);
+    getLastChange('twelve', twelve, 4);
+    getLastChange('one', one, 5);
+    getLastChange('two', two, 6);
+    getLastChange('three', three, 7);
+    getLastChange('four', four, 8);
+    getLastChange('five', five, 9);
+  }
+
+  init();
+
+  var currentDay = dayjs().format("dddd, MMMM D");
+
+  console.log(currentDay);
+  console.log($("#currentDay"));
+  $("#currentDay").append(currentDay);
+
+  var eight = ("#hour-8");
+  var nine = ("#hour-10");
+  var ten = ("#hour-9");
+  var eleven = ("#hour-11");
+  var twelve = ("#hour-12");
+  var one = ("#hour-1");
+  var two = ("#hour-2");
+  var three = ("#hour-3");
+  var four = ("#hour-4");
+  var five = ("#hour-5");
+
+  var time = dayjs().format('H');
+
+  if(time < 8) {
+    eight.addClass('future').removeClass('past', 'present')
+    nine.addClass('future').removeClass('past', 'present')
+    ten.addClass('future').removeClass('past', 'present')
+    eleven.addClass('future').removeClass('past', 'present')
+    twelve.addClass('future').removeClass('past', 'present')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 8) {
+    eight.addClass('present').removeClass('past', 'future')
+    nine.addClass('future').removeClass('past', 'present')
+    ten.addClass('future').removeClass('past', 'present')
+    eleven.addClass('future').removeClass('past', 'present')
+    twelve.addClass('future').removeClass('past', 'present')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 9) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('present').removeClass('past', 'future')
+    ten.addClass('future').removeClass('past', 'present')
+    eleven.addClass('future').removeClass('past', 'present')
+    twelve.addClass('future').removeClass('past', 'present')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 10) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('future').removeClass('present', 'future')
+    ten.addClass('present').removeClass('past', 'future')
+    eleven.addClass('future').removeClass('past', 'present')
+    twelve.addClass('future').removeClass('past', 'present')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 11) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('present').removeClass('past', 'future')
+    twelve.addClass('future').removeClass('past', 'present')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 12) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('present').removeClass('past', 'future')
+    one.addClass('future').removeClass('past', 'present')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 13) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('present').removeClass('past', 'future')
+    two.addClass('future').removeClass('past', 'present')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 14) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('past').removeClass('present', 'future')
+    two.addClass('present').removeClass('past', 'future')
+    three.addClass('future').removeClass('past', 'present')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 15) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('past').removeClass('present', 'future')
+    two.addClass('past').removeClass('present', 'future')
+    three.addClass('present').removeClass('past', 'future')
+    four.addClass('future').removeClass('past', 'present')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 16) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('past').removeClass('present', 'future')
+    two.addClass('past').removeClass('present', 'future')
+    three.addClass('past').removeClass('present', 'future')
+    four.addClass('present').removeClass('past', 'future')
+    five.addClass('future').removeClass('past', 'present')
+  }
+
+  if(time == 17) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('past', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('past', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('past').removeClass('present', 'future')
+    two.addClass('past').removeClass('present', 'future')
+    three.addClass('past').removeClass('present', 'future')
+    four.addClass('past').removeClass('present', 'future')
+    five.addClass('present').removeClass('past', 'future')
+  }
+  
+  if(time > 17) {
+    eight.addClass('past').removeClass('present', 'future')
+    nine.addClass('past').removeClass('present', 'future')
+    ten.addClass('past').removeClass('present', 'future')
+    eleven.addClass('past').removeClass('present', 'future')
+    twelve.addClass('past').removeClass('present', 'future')
+    one.addClass('past').removeClass('present', 'future')
+    two.addClass('past').removeClass('present', 'future')
+    three.addClass('past').removeClass('present', 'future')
+    four.addClass('past').removeClass('present', 'future')
+    five.addClass('past').removeClass('present', 'future')
+  }
 });
